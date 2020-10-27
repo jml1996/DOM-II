4901698eee71423844fa4b074d8fdce73495048e
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2020-10-27 19:50:16 -0400

    Late start again. Starting now.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-// Your code goes here
+// // Your code goes here
+
+// const navButtons = document.querySelectorAll(".nav-link");
+
+// navButtons.addEventListener("mouseover", function(event) {
+
+// });
+
+// const launchButton = document.querySelector("#launchButton");
+// const confirmButton = document.querySelector("#confirmButton");
+// const cancelButton = document.querySelector("#cancelButton");
+
+// // B- MESSAGES
+// const successMessage = document.querySelector(".success");
+// const failureMessage = document.querySelector(".failure");
+
+// // C- MODAL
+// const modal = document.querySelector(".modal");
+
+// // 👉 TASK 2- Demo handling click events on button#launchButton, using:
+// //  A- HTML inside index.html
+
+// //  B- The DOM's element.onclick attribute
+// launchButton.onclick = function (event) {
+//     // future code that should run when the user clicks
+//     // console.log("about to smash the oldschool method");
+// }
+// //  C- element.addEventListener('click', callback)
+// launchButton.addEventListener('click', function (event) {
+//     // console.log('the better way');
+// });
+// launchButton.addEventListener('click', function (event) {
+//     // console.log('the better way 2');
+// });
