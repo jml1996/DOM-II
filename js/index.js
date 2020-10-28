f6711a67c51e0fa7b78085887b5326e5970eb76b
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2020-10-28 01:06:49 -0400

    Finished MVP. May have entered into stretch territory as well, but it's late, so I'm not going to continue.

--- js/index.js
+++ js/index.js
@@ -1,34 +1,79 @@
-// // Your code goes here
+// Your code goes here
 
-// const navButtons = document.querySelectorAll(".nav-link");
 
-// navButtons.addEventListener("mouseover", function(event) {
+// I realize this is a bit of a mess, but it's late, and I was
+// to maximize the variety of my event handlers rather than
+// make them as consistent and neat as possible.
 
-// });
+const navButtons = document.querySelectorAll(".nav-link");
+const arrayOfNavButtons = Array.from(navButtons);
+let originalFont = "";
+arrayOfNavButtons.forEach((button) => button.addEventListener('mouseover', (event) => {
+    button.style.fontFamily = "'Times New Roman', Times, serif";
+}));
+arrayOfNavButtons.forEach((button) => button.addEventListener('mouseout', (event) => {
+        button.style.fontFamily = originalFont;
+}));
 
-// const launchButton = document.querySelector("#launchButton");
-// const confirmButton = document.querySelector("#confirmButton");
-// const cancelButton = document.querySelector("#cancelButton");
+const busImg = document.querySelector(".fun-bus-img");
+let originalSize = busImg.style.width;
 
-// // B- MESSAGES
-// const successMessage = document.querySelector(".success");
-// const failureMessage = document.querySelector(".failure");
+console.log(`og: ${originalSize}`);
+// Bus Image
+// (Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/wheel_event)
+function zoom(event) {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    busImg.style.transform = `scale(${scale})`;
+  }
+let scale = 1;
+busImg.onwheel = zoom;
 
-// // C- MODAL
-// const modal = document.querySelector(".modal");
+document.addEventListener('click', () => busImg.style.transform = `scale(${1})`);
 
-// // 👉 TASK 2- Demo handling click events on button#launchButton, using:
-// //  A- HTML inside index.html
+const signMeUpBtns = document.querySelectorAll(".btn");
+const arraySignMeUpBtns = Array.from(signMeUpBtns);
 
-// //  B- The DOM's element.onclick attribute
-// launchButton.onclick = function (event) {
-//     // future code that should run when the user clicks
-//     // console.log("about to smash the oldschool method");
-// }
-// //  C- element.addEventListener('click', callback)
-// launchButton.addEventListener('click', function (event) {
-//     // console.log('the better way');
-// });
-// launchButton.addEventListener('click', function (event) {
-//     // console.log('the better way 2');
-// });
+let originalBack = arraySignMeUpBtns[0].style.background;
+let originalColor = arraySignMeUpBtns[0].style.color;
+arraySignMeUpBtns.forEach((button) => button.setAttribute("tabindex", 0));
+arraySignMeUpBtns.forEach((button) => button.addEventListener('focus', () => {
+    button.style.background = 'red';
+}));
+
+document.addEventListener('keydown', () => arraySignMeUpBtns.forEach((button) => {
+    button.style.background = 'yellow';
+    button.style.color = 'black';
+}));
+document.addEventListener('keyup', () => arraySignMeUpBtns.forEach((button) => {
+    button.style.background = originalBack;
+    button.style.color = originalColor;
+}));
+
+const imgs = document.querySelectorAll("img");
+const imgArray = Array.from(imgs);
+
+var dragOrigin;
+imgArray.forEach((img) => {
+    img.addEventListener("dragstart", (event) => {
+        dragOrigin = event.target;
+    }, false);
+    document.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+    document.addEventListener("dragenter", function(event) {
+        event.target.style.background = "purple";
+    }, false);
+    document.addEventListener("dragleave", function(event) {
+        event.target.style.background = "";
+    }, false);
+    document.addEventListener("drop", (event) => {
+        event.preventDefault();
+        if(event.target.nodeName !== "IMG"){
+            dragOrigin.parentNode.removeChild(dragOrigin);
+            event.target.appendChild(dragOrigin);
+            event.target.style.background = "";
+        }
+    }, false);
+});
